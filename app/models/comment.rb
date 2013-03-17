876c72bb4ce9ad05687ba183eb94a4cc5609a4bc
thiyagarajan <thiyagarajannv@yahoo.com>
2013-03-17 18:54:14 +0530

    added partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :recipe
   attr_accessible :commenter, :body, :recipe_id
-  validates :commenter, :presence => true
-  validates :body, :presence => true,
-            :length => {:minimum => 50}
+  validates_presence_of :commenter, :message => "Your Commenter can't be blank"
+  validates_presence_of :body, :message => "Your body can't be blank"
 end

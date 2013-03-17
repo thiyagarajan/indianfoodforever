79843f4d2e655ea1b9cf8d6f8edbfbf19bc44d81
thiyagarajan <thiyagarajannv@yahoo.com>
2013-03-17 12:42:05 +0530

    added description

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,16 +12,26 @@ require 'pp'
 
 i=1
 doc = Nokogiri::HTML(open('http://www.indianfoodforever.com/'))
+
 results = doc.css('div.top-link a').map do |cname|
   Category.create([name: cname.content])
   recipe = cname.attribute('href').to_s
-  puts recipe
+  #puts recipe
   doc1 = Nokogiri::HTML(open(recipe))
   #puts doc1
   results1 = doc1.css('div.mainlinks a').map do |rece|
 
-    Recipe.create([category_id: i, name: rece.content])
-    puts rece.content
+    #Recipe.create([category_id: i, name: rece.content])
+    #puts rece.content
+    recipe1 = rece.attribute('href').to_s
+    puts recipe1
+    doc2 = Nokogiri::HTML(open(recipe1))
+    results2 = doc2.css('body').map do |rece1|
+      ingredients = rece1.css('div.text')
+      description = rece1.css('ul')
+      #puts ingredients.inner_html
+      Recipe.create([category_id: i, name: rece.content, ingredients: ingredients.inner_html, description: description.inner_html])
+    end
   end
   i=i+1
 end
